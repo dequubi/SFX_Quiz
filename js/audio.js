3e1e416aa6951a8aa10af4344811339cee4b867d
Alexey Zezin <alexey.zezin@gmail.com>
2021-05-03 17:40:28 +0300

    Added remaining sounds and the spoiler button

--- js/audio.js
+++ js/audio.js
@@ -7,6 +7,7 @@ let area = document.querySelectorAll('textarea');
 let counter = 0;
 let counterHTML = document.getElementById('counter');
 let valid;
+let spoiler = document.getElementById('reveal');
 
 area.forEach(function(item, i, arr) {
   item.addEventListener('input', function() {
@@ -57,19 +58,19 @@ area.forEach(function(item, i, arr) {
         valid = ['Devil May Cry 5', ['dmc5', 'dmc 5', 'devil may cry 5']];
         break;
       case '16':
-        valid = ['placeh', ['placeh']];
+        valid = ['Deus Ex: Human Revolution', ['dehr', 'deus ex human revolution', 'deus ex: human revolution']];
         break;
       case '17':
-        valid = ['placeh', ['placeh']];
+        valid = ['Among Us', ['amongus', 'among us', 'amogus']];
         break;
       case '18':
-        valid = ['placeh', ['placeh']];
+        valid = ['NieR:Automata™', ['nier automata', 'nier: automata']];
         break;
       case '19':
-        valid = ['placeh', ['placeh']];
+        valid = ['Noita', ['noita']];
         break;
       case '20':
-        valid = ['placeh', ['placeh']];
+        valid = ['Fall Guys: Ultimate Knockout', ['fallguys', 'fall guys']];
         break;
       default:
         break;
@@ -82,7 +83,99 @@ function checkAndSet(validArray, item, correctName) {
   if (validArray[1].some(v => item.value.includes(v))) {
     item.value = validArray[0];
     item.disabled = true;
+    item.classList.add('correct');
     counter++;
     counterHTML.textContent = counter;
+    if (counter==20) {
+      counterHTML.classList.add('completed');
+    }
   }
+}
+
+let start;
+let end;
+
+spoiler.onmousedown = function() {
+  start = new Date();
+}
+
+spoiler.onmouseup = function() {
+  end = new Date();
+  let delta = end - start;
+  if (delta > 2000)
+    area.forEach(function(item, i, arr) {
+      if (!item.classList.contains('correct')) {
+        switch (item.parentNode.id) {
+          case '1':
+            valid = 'HITMAN™';
+            break;
+          case '2':
+            valid = 'Jump King';
+            break;
+          case '3':
+            valid = 'Diablo 2';
+            break;
+          case '4':
+            valid = 'TES IV: Oblivion';
+            break;
+          case '5':
+            valid = 'God of War (2005)';
+            break;
+          case '6':
+            valid = 'Worms Armageddon';
+            break;
+          case '7':
+            valid = 'Gothic 3';
+            break;
+          case '8':
+            valid = 'Subnautica';
+            break;
+          case '9':
+            valid = 'Dark Souls';
+            break;
+          case '10':
+            valid = 'The Last of Us';
+            break;
+          case '11':
+            valid = 'Bloodborne';
+            break;
+          case '12':
+            valid = 'Yakuza 0';
+            break;
+          case '13':
+            valid = 'Naruto: Ultimate Ninja Storm';
+            break;
+          case '14':
+            valid = 'DOOM (2016)';
+            break;
+          case '15':
+            valid = 'Devil May Cry 5';
+            break;
+          case '16':
+            valid = 'Deus Ex: Human Revolution';
+            break;
+          case '17':
+            valid = 'Among Us';
+            break;
+          case '18':
+            valid = 'NieR:Automata™';
+            break;
+          case '19':
+            valid = 'Noita';
+            break;
+          case '20':
+            valid = 'Fall Guys: Ultimate Knockout';
+            break;
+          default:
+            break;
+        }
+        setSpoilers(valid, item);
+      }
+    });
+}
+
+function setSpoilers(valid, item) {
+  item.value = valid;
+  item.disabled = true;
+  item.classList.add('spoiler');
 }
